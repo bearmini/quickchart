a48f8793e1f25748c4aa38f1ce00377b4bc7a3f5
Takashi Oguma <oguma@soracom.jp>
2020-10-22 12:27:57 +0000

    add functions

--- chart.go
+++ chart.go
@@ -2,6 +2,7 @@ package quickchart
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/url"
 )
 
@@ -11,6 +12,14 @@ type Chart struct {
 	Options *ChartOptions `json:"options,omitempty"`
 }
 
+func NewChart(t ChartType, d ChartData, o *ChartOptions) *Chart {
+	return &Chart{
+		Type:    t,
+		Data:    d,
+		Options: o,
+	}
+}
+
 func (c *Chart) Encode() (string, error) {
 	b, err := json.Marshal(c)
 	if err != nil {
@@ -18,4 +27,13 @@ func (c *Chart) Encode() (string, error) {
 	}
 
 	return url.QueryEscape(string(b)), nil
+}
+
+func (c *Chart) GetURL() (string, error) {
+	encodedChart, err := c.Encode()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("https://quickchart.io/chart?c=%s", encodedChart), nil
 }
