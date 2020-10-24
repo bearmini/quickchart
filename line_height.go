018b56e4ba3632d862527230037721fbde3f8b46
Takashi Oguma <oguma@soracom.jp>
2020-10-24 11:46:28 +0000

    update scale label options

---
+++ line_height.go
@@ -0,0 +1,7 @@
+package quickchart
+
+type LineHeight interface{}
+
+type LineHeightNumber float64
+
+type LineHeightString string
--- scale_label_options.go
+++ scale_label_options.go
@@ -1,8 +1,12 @@
 package quickchart
 
 type ScaleLabelOptions struct {
-	Display     *bool   `json:"display,omitempty"`
-	LabelString *string `json:"labelString,omitempty"`
-	Font        *Font   `json:"font,omitempty"`
-	Padding     Padding `json:"padding,omitempty"`
+	Display     *bool      `json:"display,omitempty"`
+	FontSize    *float64   `json:"fontSize,omitempty"`
+	FontFamily  *string    `json:"fontFamily,omitempty"`
+	FontColor   Color      `json:"fontColor,omitempty"`
+	FontStyle   *string    `json:"fontStyle,omitempty"`
+	LabelString *string    `json:"labelString,omitempty"`
+	LineHeight  LineHeight `json:"lineHeight,omitempty"`
+	Padding     Padding    `json:"padding,omitempty"`
 }
